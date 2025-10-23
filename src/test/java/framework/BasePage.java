623d3a3ac9d7cace6f42585ca6ada1d9868334e8
lisettcastillomendoza <lisettcastillomendoza@gmail.com>
2025-10-23 15:26:26 -0300

    para el funcionamiento de jenkins use maxime

--- src/test/java/framework/BasePage.java
+++ src/test/java/framework/BasePage.java
@@ -43,6 +43,7 @@ public class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().window().maximize();
     }
 
     public WebDriver getDriver() {
